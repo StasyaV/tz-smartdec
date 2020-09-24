57b0655a4689c68d5ed942abc1efb980b0745707
StasyaV <stasya522@mail.ru>
2020-09-24 14:29:38 +0300

    Стилизация мобилки

--- source/js/script.js
+++ source/js/script.js
@@ -1,24 +1,13 @@
 'use strict';
 
 window.addEventListener("load", function () {
-  // var mainMenu = document.querySelector('.main-nav');
-  // var menuToggle = document.querySelector('.menu-button');
+  var menu = document.querySelector('.header__navigation');
+  var menuToggle = document.querySelector('.menu-button');
 
-  // if (menuToggle) {
-  //   mainMenu.classList.remove('main-nav--no-js');
-  //   menuToggle.classList.remove('menu-button--none');
-
-  //   menuToggle.addEventListener('click', function () {
-  //     if (mainMenu.classList.contains('main-nav--closed')) {
-  //       mainMenu.classList.remove('main-nav--closed');
-  //       mainMenu.classList.add('main-nav--open');
-  //       menuToggle.classList.add('menu-button--open');
-  //     } else {
-  //       mainMenu.classList.add('main-nav--closed');
-  //       mainMenu.classList.remove('main-nav--open');
-  //       menuToggle.classList.remove('menu-button--open');
-  //       menuToggle.classList.add('menu-button--closed');
-  //     }
-  //   });
-  // }
+  if (menuToggle) {
+    menuToggle.addEventListener('click', function () {
+      menuToggle.classList.toggle('menu-button--closed');
+      menu.classList.toggle('header__navigation--close');
+    });
+  }
 });
